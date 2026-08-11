b9656cd99a83b022c5a4afac8d7e0bb2825c2941
suryaprakash <b.7993974026@gmail.com>
2026-08-11 13:58:53 +0000

    c

---
+++ print.c
@@ -0,0 +1,10 @@
+#include <stdio.h>  
+void main(){
+    char s;
+    scanf("%c",&s); // we are taking input from the user 
+
+    printf("Hello, World!\n");// we have to use \n whenever we want to jump to next line
+    printf("%c\n", s); // we are printing the value in s
+
+ 
+}
---
+++ print1.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+int main() {
+    char ch;
+    char s[100];
+    char sen[100];
+    
+    scanf("%c", &ch);
+    scanf("%s", s);
+    scanf("\n");          // Clear the leftover newline
+    scanf("%[^\n]", sen);
+    
+    printf("%c\n%s\n%s", ch, s, sen);
+    
+    return 0;
+}
